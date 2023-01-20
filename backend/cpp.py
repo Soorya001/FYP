c853a409b2c990553c26e7f511ebc8b6664394ba
Madhav007 <madhavaprashath669@gmail.com>
2023-01-20 14:30:31 +0530

    Blocks {}  and Declaration Statements done

--- backend/cpp.py
+++ backend/cpp.py
@@ -1,3 +1,4 @@
+dataTypesAvailable = {'character':'char', 'boolean':'bool', 'integer':'int', 'float':'float', 'decimal':'decimal','string':'string'}  # Primitive data types only
 def for_function(varName,start,end,step):
     startNo = int(start)
     endNo = int(end)
@@ -9,3 +10,19 @@ def for_function(varName,start,end,step):
 
 def headerFile(fileName):
     return f'#include<{fileName}>'
+
+def declaration(varName, userDataType, varValue):
+    dataType=""
+    if(userDataType in dataTypesAvailable):
+        dataType = dataTypesAvailable[userDataType]
+    # else:                                            Should implement the case when none of the primitive data types match with userDataType
+    #     dataType="void"
+
+    declarationStatement = ""
+
+    if(dataType=="string"):
+        declarationStatement = f"{dataType} {varName} = \"{varValue}\";\n"
+    else:
+        declarationStatement = f"{dataType} {varName} = {varValue};\n"
+
+    return declarationStatement
